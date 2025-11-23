8f8b3b22cbde306f94cbcbdd3c5210a5749f04d7
ItIsWhatItIs7000 <poa27584@gmail.com>
2025-11-23 03:02:16 +0000

    Created abstract Book w/ Constructor and getters and title/author/genre

---
+++ src/Book.java
@@ -0,0 +1,21 @@
+public abstract class Book{
+    private String title;
+    private String author;
+    private Genre genre;
+    public Book(String title, String author, Genre genre){
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+    public Genre getGenre(){
+        return genre;
+    }
+}
