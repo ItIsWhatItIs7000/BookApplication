63e8ff30e8d5b5fb4df6b78de31dc726ced9c30c
ItIsWhatItIs7000 <poa27584@gmail.com>
2025-11-23 18:43:43 +0000

    Added a Test (TestBook) to test if Book is working as intended.

---
+++ src/TestBook.java
@@ -0,0 +1,14 @@
+public class TestBook{
+    public static void main(String[] args){
+        System.out.println("Testing Genre.java");
+        Genre fiction = Genre.FICTION;
+        System.out.println(" genre: " + fiction);
+        Genre unknown  = Genre.fromString("blahblahtesting testing");
+        System.out.println("This random thing should default to: " + unknown);
+        System.out.println("We gonna test Book using a rreal book class now.");
+        // we will need a real type of book later
+        System.out.println("Book is abstractwewill test with PrintedBook later");
+         System.out.println("everything in order man");
+
+    }
+}
